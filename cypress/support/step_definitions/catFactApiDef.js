52b5325ad3f041ce223b1199edd7f6ad99219b76
StrahinjaBecagol <strahinja.becagol@vegaitglobal.com>
2025-12-11 11:48:22 +0100

    Added first api key

---
+++ cypress/support/step_definitions/catFactApiDef.js
@@ -0,0 +1,27 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+let baseUrl = "https://catfact.ninja";
+
+Given("the fact endpoint", () => {
+    cy.wrap(`${baseUrl}/fact`).as("endpoint");
+
+});
+
+When('a request is made', () => {
+    cy.get("@endpoint").then((endpoint) => {
+        cy.request(endpoint).as("apiResponse");
+    })
+
+});
+
+Then('it should return a single cat fact with valid structure', () => {
+    cy.get("@apiResponse").then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('fact');
+        expect(response.body).to.have.property('length');
+        expect(response.body.fact).to.be.a('string');
+        expect(response.body.length).to.be.a('number');
+    });
+});
+
+
+
